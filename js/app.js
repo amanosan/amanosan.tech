37f906d822167eb3a3f716f6853c55be1f12c02e
amanosan121 <amanosan121@gamil.com>
2022-02-18 17:52:53 +0530

    added JS

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// window.particleJS("particles-js", "particlejs-config.json");
+
+// particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+//     console.log('callback - particles.js config loaded');
+// });
+
+// navigation
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__menu");
+const navSocials = document.querySelector(".nav__socials")
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navSocials.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    navSocials.classList.remove("active");
+}
